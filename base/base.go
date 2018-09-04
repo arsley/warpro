4ef16396a153edf02c5e30725b3899121351c9ba
assly <blue20will@gmail.com>
2018-09-04 21:28:46 +0900

    add: Create base.go (inputs) template

---
+++ base/base.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func gets() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func toInt(value string) int {
+	num, _ := strconv.Atoi(value)
+	return num
+}
+
+func splitWithInt(value string) []int {
+	vals := strings.Split(value, " ")
+	nums := make([]int, len(vals))
+	for index, elem := range vals {
+		nums[index] = toInt(elem)
+	}
+	return nums
+}
+
+func main() {
+
+}
