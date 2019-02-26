030e9b75782fd94e8c5e6362305946e1b80c1e5c
arsley <blue20will@gmail.com>
2019-02-26 18:37:08 +0900

    add: Add AtCoder/ABC118 AB

---
+++ atcoder/abc118/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func gets() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func toInt(value string) int {
+	num, _ := strconv.Atoi(value)
+	return num
+}
+
+func splitWithInt(value string) []int {
+	vals := strings.Split(value, " ")
+	nums := make([]int, len(vals))
+	for index, elem := range vals {
+		nums[index] = toInt(elem)
+	}
+	return nums
+}
+
+// A
+// func main() {
+// 	inputs := splitWithInt(gets())
+// 	A, B := inputs[0], inputs[1]
+// 	if B % A == 0 {
+// 		fmt.Println(A + B)
+// 	} else {
+// 		fmt.Println(B - A)
+// 	}
+// }
+
+// B
+func main() {
+	var inputs []int
+	inputs = splitWithInt(gets())
+	N, M := inputs[0], inputs[1]
+	countArray := make([]int, M)
+	for i:= 0; i < N; i++ {
+		inputs = splitWithInt(gets())[1:]
+		for _, elem := range inputs {
+			countArray[elem - 1]++
+		}
+	}
+
+	countKinds := 0
+	for _, elem := range countArray {
+		if elem == N { countKinds++ }
+	}
+
+	fmt.Println(countKinds)
+}
