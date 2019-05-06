09d9869f4dd8c353a7f69373bcddcd832aa67e08
arsley <blue20will@gmail.com>
2019-05-07 00:23:44 +0900

    add: ABC123 AB (with remove swap file)

--- atcoder/abc123/main.rb
+++ atcoder/abc123/main.rb
@@ -1,5 +1,11 @@
 # A
+# inputs = []
+# 5.times { inputs << gets.to_i }
+# k = gets.to_i
+# puts inputs.combination(2).map { |e| e[1] - e[0] }.any? { |e| e > k } ? ':(' : 'Yay!'
+
+# B
 inputs = []
 5.times { inputs << gets.to_i }
-k = gets.to_i
-puts inputs.combination(2).map { |e| e[1] - e[0] }.any? { |e| e > k } ? ':(' : 'Yay!'
+puts inputs.reduce(0) { |mem, e| e % 10 == 0 ? mem + e : mem + e + (10 - e % 10) } - inputs.map { |e| e % 10 == 0 ? 0 : 10 - e % 10 }.max
+
