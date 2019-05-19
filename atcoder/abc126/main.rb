90fca0488e7723954c854d4c87ed38f4ceed322f
arsley <blue20will@gmail.com>
2019-05-19 22:42:38 +0900

    :heavy_check_mark: ABC126 B(improve) C(WA)

--- atcoder/abc126/main.rb
+++ atcoder/abc126/main.rb
@@ -9,11 +9,11 @@ h = s[0..1].to_i
 t = s[2..3].to_i
 m = 1..12
 case
-when (h > 0 && m.include?(t)) && (t > 0 && m.include?(h))
+when (h >= 0 && m.include?(t)) && (t >= 0 && m.include?(h))
   puts 'AMBIGUOUS'
-when h > 0 && m.include?(t)
+when h >= 0 && m.include?(t)
   puts 'YYMM'
-when t > 0 && m.include?(h)
+when t >= 0 && m.include?(h)
   puts 'MMYY'
 else
   puts 'NA'
