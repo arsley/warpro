0ff82b1cf7318bd468290487ae6a1b7d98c5d939
arsley <blue20will@gmail.com>
2019-05-19 21:48:42 +0900

    :construction: ABC126 AB

---
+++ atcoder/abc126/main.rb
@@ -0,0 +1,20 @@
+# A
+# _, k = gets.chomp.split.map(&:to_i)
+# s = gets
+# puts s.tap { |w| w[k - 1] = w[k - 1].downcase }
+
+# B
+s = gets
+h = s[0..1].to_i
+t = s[2..3].to_i
+m = 1..12
+case
+when (h > 0 && m.include?(t)) && (t > 0 && m.include?(h))
+  puts 'AMBIGUOUS'
+when h > 0 && m.include?(t)
+  puts 'YYMM'
+when t > 0 && m.include?(h)
+  puts 'MMYY'
+else
+  puts 'NA'
+end
